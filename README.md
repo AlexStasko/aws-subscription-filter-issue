fe4f2f1ee13ac1cad2969b4676382a39d6223ebe
Aleksandr Stasko <aleksandr_stasko@epam.com>
2022-09-01 21:51:25 +0300

    implemented fix

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 
 ## Steps to reproduce
 
+* `git checkout e7c4f87` checkout to commit to reproduce issue
 * `cdk deploy StreamStack` deploy stack with Kinesis Stream
 * `cdk deploy AppStack` deploy stack with app
+
+## Steps to fix
+
+* `git checkout main` checkout to the latest commit with fix
+* `cdk deploy StreamStack` deploy stack with Kinesis Stream
+* `cdk deploy AppStack` deploy stack with app
+
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,8 +1,8 @@
-import { Stack, StackProps, Fn, RemovalPolicy } from "aws-cdk-lib";
+import { Stack, StackProps, Fn, RemovalPolicy, Resource } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { KinesisDestination } from "aws-cdk-lib/aws-logs-destinations";
 import { Stream } from "aws-cdk-lib/aws-kinesis";
-import { FilterPattern, LogGroup } from "aws-cdk-lib/aws-logs";
+import { FilterPattern, LogGroup, SubscriptionFilterProps, CfnSubscriptionFilter } from "aws-cdk-lib/aws-logs";
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -13,9 +13,42 @@ export class AppStack extends Stack {
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    logGroup.addSubscriptionFilter('Filter', {
-      destination: new KinesisDestination(Stream.fromStreamArn(this, 'Stream', Fn.importValue('test-stream-arn'))),
+    new FixedSubscriptionFilter(this, 'Filter', {
+      logGroup,
+      destination: new KinesisDestination(
+        Stream.fromStreamArn(this, 'Stream', Fn.importValue('test-stream-arn'))),
       filterPattern: FilterPattern.allEvents(),
     });
   }
 }
+/**
+  * This class is copy of {@link SubscriptionFilter}
+  * with fix added in the end of constructor
+  */
+class FixedSubscriptionFilter extends Resource {
+  constructor(scope: Construct, id: string, props: SubscriptionFilterProps) {
+    super(scope, id);
+
+    const destProps = props.destination.bind(this, props.logGroup);
+
+    const filter = new CfnSubscriptionFilter(this, 'Resource', {
+      logGroupName: props.logGroup.logGroupName,
+      destinationArn: destProps.arn,
+      roleArn: destProps.role && destProps.role.roleArn,
+      filterPattern: props.filterPattern.logPatternString,
+    });
+
+    // Add dependency on policy
+    if (destProps.role) {
+      // Find policy attached to role
+      const policy = destProps.role.node.tryFindChild('DefaultPolicy');
+      if (policy) {
+        // filter should depend on policy
+        filter.node.addDependency(policy);
+      } else {
+        throw new Error('there is no policy');
+      }
+    }
+  }
+}
+
